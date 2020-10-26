2fad5cc87b8bde980ac16c2bc3c2b098a4d3a957
Bartek-1111 <62350242+Bartek-1111@users.noreply.github.com>
2020-10-26 17:43:56 +0100

    Wersja 2.0 (#1)
    W pełni działająca obsługa podstawowej gry w O i X dla 2 graczy. Brak klas.

--- main/TicTacToe_tryA.cpp
+++ main/TicTacToe_tryA.cpp
@@ -2,7 +2,45 @@
 
 using namespace std;
 
-void draw_board(char in[3][3])
+bool end_condition(char* tab, char in)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        if ((*(tab + i) == in) && (*(tab + i + 3) == in) && (*(tab + i + 6) == in))
+        {
+            cout << "Wygrana pionowo.\n";
+            return true;
+        }
+        if ((*(tab + i * 3) == in) && (*(tab + i * 3 + 1) == in) && (*(tab + i * 3 + 2) == in))
+        {
+            cout << "Wygrana poziomo.\n";
+            return true;
+        }
+    }
+    if ((*(tab) == in) && (*(tab + 4) == in) && (*(tab + 8) == in))
+    {
+        cout << "Wygrana na skos.\n";
+        return true;
+    }
+    if ((*(tab + 2) == in) && (*(tab + 4) == in) && (*(tab + 6) == in))
+    {
+        cout << "Wygrana na skos.\n";
+        return true;
+    }
+    return false;
+}
+
+bool set_cell(char* tab, int size_x, int size_y, int target_pos, char in)
+{
+    const int size = size_x * size_y;
+    //cout << "Wypelnianie " << target_pos << '/' << size << " tablicy.\n";
+    if (target_pos >= size) return false;
+    if (*(tab + target_pos) == '_') *(tab + target_pos) = in;
+    else return false;
+    return true;
+}
+
+void draw_board(char* in)
 {
     cout << "_|A|B|C|\n";
     for (int i = 0; i < 3; i++)
@@ -11,44 +49,45 @@ void draw_board(char in[3][3])
         for (int j = 0; j < 3; j++)
         {
             //cout << i << "-" << j << "\n";
-            cout << in[i][j] << "|";
+            cout << *(in + i * 3 + j) << "|";
         }
         cout << "\n";
     }
 }
 
-void draw(char in_a[3][3], bool in_b)
+void draw(char* in_a, int tura)
 {
     system("cls");
-    cout << "Tic tac toe v1.0\n";
+    cout << "Tic tac toe v2.0\n";
     cout << "Podaj pole (np. A3 lub a3).\n";
-    cout << "Grasz jako ";
-    if (in_b == 1) cout << "X.\n";
-    else cout << "O.\n";
+    cout << "Tura:\t" << tura << ".\n";
+    cout << "Grasz jako " << ((tura % 2 != 0) ? 'X' : 'O') << ".\n";
     draw_board(in_a);
 }
 
-//ta funkcja ma "dostać" tablicę 'board' i dodać do niej X lub O na właściwe miejsce
-void fill_array(string in, bool in_b)
+int get_pos(string in)
 {
+    int target_pos = 0;
     if ((in[0] > 64) && (in[0] < 68))
     {
         //a,b,c = 65, 66, 67
+        target_pos += in[0] - 65;
     }
     else
     {
         //a,b,c = 97, 98, 99
+        target_pos += in[0] - 97;
     }
     //1,2,3 = 49, 50, 51
+    target_pos += 3 * (in[1] - 49);
+    return target_pos;
 }
 
 int main()
 {
     char board[3][3];
     string odp;
-    char odpA;
-    int odpB;
-    bool gra_x = true;
+    int tura = 1;
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
@@ -56,11 +95,11 @@ int main()
             board[i][j] = '_';
         }
     }
+    draw(&board[0][0], tura);
     while (1)
     {
         while (1)
         {
-            draw(board, gra_x);
             cout << "Podaj pole:";
             cin >> odp;
             if (((odp[0] > 64) && (odp[0] < 68)) || ((odp[0] > 96) && (odp[0] < 100)))
@@ -71,14 +110,29 @@ int main()
                 }
             }
         }
-
-        cout << "Brawo, poprawne dane!\n";
-        system("PAUSE");
-        gra_x = !gra_x;
-        //ciąg dalszy programu.
+        if (!set_cell(&board[0][0], 3, 3, get_pos(odp), (tura % 2 != 0) ? 'X' : 'O'))
+        {
+            cout << "Pole zajete.\n";
+            system("PAUSE");
+            tura--;
+        }
+        tura++;
+        draw(&board[0][0], tura);
+        if (end_condition(&board[0][0], (tura % 2 == 0) ? 'X' : 'O'))
+        {
+            tura--;
+            draw(&board[0][0], tura);
+            cout << "Wygral gracz " << ((tura % 2 != 0) ? 'X' : 'O') << "!\n";
+            system("PAUSE");
+            return 0;
+        }
+        if (tura > 9)
+        {
+            tura--;
+            draw(&board[0][0], tura);
+            cout << "Koniec gry. Wypelniono wszystkie pola. Remis.\n";
+            system("PAUSE");
+            return 0;
+        }
     }
-
-
-    draw(board, gra_x);
-    system("PAUSE");
 }
